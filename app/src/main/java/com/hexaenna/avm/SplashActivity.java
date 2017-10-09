a2b323939af4d4be6c3c7b80f509af00c1b8ce3e
Priya <priyadharsini586@gmail.com>
2017-10-09 12:18:27 +0530

    reg_ui_update

--- app/src/main/java/com/hexaenna/avm/SplashActivity.java
+++ app/src/main/java/com/hexaenna/avm/SplashActivity.java
@@ -4,15 +4,23 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
+    ImageView imgSplash;
+    Animation imgAnimation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.splash_activity);
+        imgSplash = (ImageView) findViewById(R.id.imgSplash);
+        imgAnimation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_splash);
+
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
