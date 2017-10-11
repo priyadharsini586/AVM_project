ff96e2b4ad93d1617853f1c1b265c95e27516bef
Priya <priyadharsini586@gmail.com>
2017-10-11 17:40:34 +0530

    repair content sales content

--- app/src/main/java/com/hexaenna/avm/CollectionRequest.java
+++ app/src/main/java/com/hexaenna/avm/CollectionRequest.java
@@ -8,21 +8,82 @@ import android.widget.LinearLayout;
 
 public class CollectionRequest extends AppCompatActivity implements View.OnClickListener {
 
-    LinearLayout ldtHome;
+    LinearLayout ldtHome,ldtCertificate,ldtOtherServices,ldtCalibration,ldtSales,ldtRepair,ldtCollectionRequest,ldtOnsiteRequest,ldtMblCalibration,ldtProduct,ldtDownload,ldtContact;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.collection_request_activity);
         ldtHome = (LinearLayout) findViewById(R.id.ldtHome);
         ldtHome.setOnClickListener(this);
+
+        ldtCertificate = (LinearLayout) findViewById(R.id.ldtCertificate);
+        ldtCertificate.setOnClickListener(this);
+
+        ldtOtherServices = (LinearLayout) findViewById(R.id.ldtOtherServices);
+        ldtOtherServices.setOnClickListener(this);
+
+        ldtCalibration = (LinearLayout) findViewById(R.id.ldtCalibration);
+        ldtCalibration.setOnClickListener(this);
+
+        ldtSales = (LinearLayout) findViewById(R.id.ldtSales);
+        ldtSales.setOnClickListener(this);
+
+        ldtRepair = (LinearLayout) findViewById(R.id.ldtRepair);
+        ldtRepair.setOnClickListener(this);
+
+        ldtCollectionRequest = (LinearLayout) findViewById(R.id.ldtCollectionRequest);
+        ldtCollectionRequest.setOnClickListener(this);
+
+        ldtOnsiteRequest = (LinearLayout) findViewById(R.id.ldtOnsiteRequest);
+        ldtOnsiteRequest.setOnClickListener(this);
+
+        ldtMblCalibration = (LinearLayout) findViewById(R.id.ldtmblCalibration);
+        ldtMblCalibration.setOnClickListener(this);
+
+        ldtProduct = (LinearLayout) findViewById(R.id.ldtProduct);
+        ldtProduct.setOnClickListener(this);
+
+        ldtDownload = (LinearLayout) findViewById(R.id.ldtDownload);
+        ldtDownload.setOnClickListener(this);
+
+        ldtContact = (LinearLayout) findViewById(R.id.ldtContactUs);
+        ldtContact.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+       Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
         switch (v.getId())
         {
+
             case R.id.ldtHome:
-                Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+                intent.putExtra("where","home");
+                startActivity(intent);
+                break;
+
+            case R.id.ldtCertificate:
+                intent.putExtra("where","certificate");
+                startActivity(intent);
+                break;
+
+            case R.id.ldtOtherServices:
+                intent.putExtra("where","otherServices");
+                startActivity(intent);
+                break;
+
+            case R.id.ldtCalibration:
+                intent.putExtra("where","calibration");
+                startActivity(intent);
+                break;
+
+            case R.id.ldtSales:
+                intent.putExtra("where","sales");
+                startActivity(intent);
+                break;
+
+            case R.id.ldtRepair:
+                intent.putExtra("where","repair");
                 startActivity(intent);
                 break;
         }
--- app/src/main/java/com/hexaenna/avm/HomeActivity.java
+++ app/src/main/java/com/hexaenna/avm/HomeActivity.java
@@ -1,25 +1,84 @@
 package com.hexaenna.avm;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity  {
 
 
     TextView txtHomeContent,txtOtherServices;
+    private WebView webHome;
+    LinearLayout ldtOtherServices;
+    String fromWhere = "";
+    ScrollView scrMainView;
+    TextView txtToolbarText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
 
-      /*  txtHomeContent = (TextView) findViewById(R.id.txtHomeContent);
-        txtOtherServices = (TextView) findViewById(R.id.txtOtherServices);
-        txtOtherServices.setText(Html.fromHtml(getString(R.string.other_services_content)));*/
+
+        scrMainView = (ScrollView) findViewById(R.id.scrMain);
+
+        txtToolbarText = (TextView) findViewById(R.id.txtToolbarText);
+        ldtOtherServices = (LinearLayout) findViewById(R.id.ldtOtherServices);
+        webHome = (WebView) findViewById(R.id.webHome);
+
+        Bundle extrasBundle = getIntent().getExtras();
+        if (extrasBundle != null)
+        {
+            Log.e("from",extrasBundle.getString("where"));
+            fromWhere = extrasBundle.getString("where");
+        }
+
+        if (fromWhere.contains("home"))
+        {
+            webHome.setVisibility(View.VISIBLE);
+            webHome.loadUrl("file:///android_asset/home.html");
+
+            ldtOtherServices.setVisibility(View.GONE);
+            webHome.setBackgroundColor(Color.TRANSPARENT);
+            txtToolbarText.setText("Home");
+        }else if (fromWhere.contains("certificate"))
+        {
+            txtToolbarText.setText("Certificate");
+            ldtOtherServices.setVisibility(View.GONE);
+            webHome.setVisibility(View.GONE);
+        }else if (fromWhere.contains("otherServices"))
+        {
+            txtToolbarText.setText("Other Services");
+            ldtOtherServices.setVisibility(View.GONE);
+            webHome.setVisibility(View.GONE);
+            ldtOtherServices.setVisibility(View.VISIBLE);
+
+        }else if (fromWhere.contains("sales")){
+            txtToolbarText.setText("Sales");
+            webHome.setVisibility(View.VISIBLE);
+            webHome.loadUrl("file:///android_asset/sales.html");
+            webHome.setBackgroundColor(Color.TRANSPARENT);
+           /* ldtOtherServices.setVisibility(View.GONE);
+            ldtCalibration.setVisibility(View.GONE);*/
+
+        }else if (fromWhere.contains("repair")){
+
+
+            webHome.setVisibility(View.VISIBLE);
+            webHome.loadUrl("file:///android_asset/repair.html");
+            txtToolbarText.setText("Repair");
+            webHome.setBackgroundColor(Color.TRANSPARENT);
+
+           /* ldtOtherServices.setVisibility(View.GONE);
+            ldtCalibration.setVisibility(View.GONE);*/
+        }
 
 
 
